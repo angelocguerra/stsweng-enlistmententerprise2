4a8ddedd290f0f50adc82d92f79b9bfc9537c897
Taco <bensonyao123@gmail.com>
2024-03-12 00:06:02 +0800

    Added necessary imports and test container in SectionControllerTest.java

--- src/integrationTest/java/com/orangeandbronze/enlistment/controllers/SectionsControllerIT.java
+++ src/integrationTest/java/com/orangeandbronze/enlistment/controllers/SectionsControllerIT.java
@@ -2,8 +2,15 @@ package com.orangeandbronze.enlistment.controllers;
 
 import com.orangeandbronze.enlistment.domain.*;
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.*;
 import org.springframework.boot.test.context.*;
+import org.springframework.jdbc.core.*;
+import org.springframework.test.annotation.*;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.*;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.*;
 
 import java.time.*;
@@ -18,12 +25,37 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @AutoConfigureMockMvc
 @Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest
 class SectionsControllerIT  {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private SectionRepository sectionRepository;
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    private final static String TEST = "test";
+
+    @Container
+    private final PostgreSQLContainer container = new PostgreSQLContainer("postgres:14").withDatabaseName(TEST).withUsername(TEST).withPassword(TEST);
+
+    @DynamicPropertySource
+    private static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> "jdbc:tc:postgresql:14:///" + TEST);
+        registry.add("spring.datasource.username", () -> TEST);
+        registry.add("spring.datasource.password", () -> TEST);
+    }
 
     @Test
     void createSection_save_to_db() throws Exception {
 
     }
+
 
 }
