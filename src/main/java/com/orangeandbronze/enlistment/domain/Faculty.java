110352266a5c4435d54dadaa270cd831b00ab213
Taco <bensonyao123@gmail.com>
2024-03-21 00:19:48 +0800

    implemented Faculty class

---
+++ src/main/java/com/orangeandbronze/enlistment/domain/Faculty.java
@@ -0,0 +1,55 @@
+package com.orangeandbronze.enlistment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+
+import static org.apache.commons.lang3.Validate.isTrue;
+import static org.apache.commons.lang3.Validate.notBlank;
+
+@Entity
+public class Faculty {
+    @Id
+    private final int facultyNumber;
+    private String firstname;
+    private String lastname;
+    @OneToMany
+    private final Collection<Section> sections = new HashSet<>();
+
+    Faculty(int facultyNumber, String firstname, String lastname) {
+        isTrue (facultyNumber >= 0,
+                "studentNumber can't be negative, was: " + facultyNumber);
+        notBlank(firstname);
+        notBlank(lastname);
+
+        this.facultyNumber = facultyNumber;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty# " + facultyNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Faculty faculty = (Faculty) o;
+        return facultyNumber == faculty.facultyNumber && Objects.equals(firstname, faculty.firstname) && Objects.equals(lastname, faculty.lastname) && Objects.equals(sections, faculty.sections);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(facultyNumber, firstname, lastname, sections);
+    }
+
+    // For JPA/Hibernate, do not delete
+    private Faculty() {
+        facultyNumber = -1;
+    }
+}
