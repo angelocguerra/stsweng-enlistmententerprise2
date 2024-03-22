fab4eea163267a9dd19af901d2bd947e02c04dd9
krizchellewong <krizchelle_danielle_wong@dlsu.edu.ph>
2024-03-22 15:10:50 +0800

    unit test for sched overlap

--- src/test/java/com/orangeandbronze/enlistment/domain/SectionTest.java
+++ src/test/java/com/orangeandbronze/enlistment/domain/SectionTest.java
@@ -21,4 +21,11 @@ class SectionTest {
         assertThrows(ScheduleConflictException.class, () -> new Section("B", DEFAULT_SUBJECT, MTH830to10, room, new Faculty(1001, "firstname", "lastname")));
     }
 
+    @Test
+    void newSection_same_room_overlap_sked() {
+        Room room = new Room("X", 10);
+        new Section("A", DEFAULT_SUBJECT, MTH830to10, room, new Faculty(1000, "firstname", "lastname"));
+        assertThrows(ScheduleConflictException.class, () -> new Section("B", DEFAULT_SUBJECT, MTH9to1030, room, new Faculty(1001, "firstname", "lastname")));
+    }
+
 }
--- src/test/java/com/orangeandbronze/enlistment/domain/TestUtils.java
+++ src/test/java/com/orangeandbronze/enlistment/domain/TestUtils.java
@@ -7,6 +7,7 @@ import static com.orangeandbronze.enlistment.domain.Days.*;
 
 public class TestUtils {
     public static final Schedule MTH830to10 = new Schedule(MTH, new Period(LocalTime.of(8, 30), LocalTime.of(10, 0)));
+    public static final Schedule MTH9to1030 = new Schedule(MTH, new Period(LocalTime.of(9, 0), LocalTime.of(10, 30)));
     public static final Schedule TF830to10 = new Schedule(TF, new Period(LocalTime.of(8, 30), LocalTime.of(10, 0)));
     public static final Schedule TF10to1130 = new Schedule(TF, new Period(LocalTime.of(10, 0), LocalTime.of(11, 30)));
     public static final String DEFAULT_SECTION_ID = "DefaultSection";
@@ -15,11 +16,7 @@ public class TestUtils {
     public static final int DEFAULT_STUDENT_NUMBER = 10;
     public static final String DEFAULT_ROOM_NAME = "X";
     public static final int DEFAULT_FACULTY_NUMBER = 1000;
- public static final Faculty DEFAULT_FACULTY = new Faculty(DEFAULT_FACULTY_NUMBER, "firstname", "lastname");
-
-//    public static Faculty newFaculty(int facultyNumber) {
-//        return new Faculty(facultyNumber);
-//    }
+    public static final Faculty DEFAULT_FACULTY = new Faculty(DEFAULT_FACULTY_NUMBER, "firstname", "lastname");
 
     public static Student newStudent(int studentNumber, Collection<Section> sections, Collection<Subject> subjectsTaken) {
         return new Student(studentNumber, "firstname", "lastname", sections, subjectsTaken);
